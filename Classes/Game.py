ac34d5beec3602ec4a7880dc27d6b5192797182e
DefinitelyRus <definitelyrus@gmail.com>
2022-01-07 21:07:22 +0800

    Added comments and removed Scene & Location classes

--- Classes/Game.py
+++ Classes/Game.py
@@ -5,6 +5,13 @@ Created on 19 Oct 2021
 '''
 
 class Game:
+    #TODO: Get Game into a usable state.
+    """
+    This contains all the data about any ongoing game.
+    To start a game, a Game object has to be created, only needing to specify
+    how many levels are to be played. The rest is handled automatically upon
+    the creation of the Game object (inside __init__).
+    """
     id = None
     length = None
     levelList = []
@@ -12,7 +19,7 @@ class Game:
     def __init__(self, gameLength):
         self.id = None
         self.length = gameLength
-        
+
         for i in range(self.length):
             self.locationList.append(Scene(Location(randomName("location"))))
 
@@ -21,27 +28,9 @@ class Game:
 
 
 
-def randomName(self, nameForWhat):  
+def randomName(self, nameForWhat):
     import random
-    
+
     locationNames = ["Test1", "Test2", "Test3"]
-    
+
     if nameForWhat == "location": return random.choice(locationNames)
-    
-class Scene:
-    levelName = None
-    levelId = None
-    sceneLocation = None
-    playerList = []
-    
-    
-    def __init__(self, location, players):
-        self.sceneLocation = location
-        self.playerList = players
-        
-class Location:
-    locationName = None
-    locationId = None
-    entityList = []
-    
-    def __init__(self, name, entities):
