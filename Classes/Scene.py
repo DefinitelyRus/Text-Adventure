6e9efe03263fac947dd27397ab81bec998f1faa2
DefinitelyRus <definitelyrus@gmail.com>
2022-01-07 21:08:31 +0800

    Improved Comments and added other temporary testing

--- Classes/Scene.py
+++ Classes/Scene.py
@@ -43,22 +43,22 @@ class Scene:
         self.playerList = players
         self.sceneEvent = event
 
-    #List of playable characters.
+    #Player OBJECT List: List of playable characters.
     def setPlayerList(self, players): self.playerList = players
     def addPlayer(self, player): self.playerList.append(player)
     def getPlayerList(self): return self.playerList
     def getPlayer(self):
         if (entityList.size() == 1): return self.__entityList[0]
 
-    #Location where the scene is taking place.
+    #Location OBJECT: Location where the scene is taking place.
     def setLocation(self, location): self.sceneLocation = location
     def getLocation(self): return self.sceneLocation
 
-    #The current level# the scene is at. (unsigned integer)
+    #INT: The current level# the scene is at.
     def setLevel(self, level): self.sceneLevel = level
     def getLevel(self): return self.sceneLevel
 
-    #The event in the scene. (string) (e.g. Battle, Loot, Puzzle, Store, Talk)
+    #STRING: The event in the scene. (e.g. Battle, Loot, Puzzle, Store, Talk)
     def setEvent(self, event): self.sceneEvent = event
     def getEvent(self): return self.sceneEvent
 
@@ -67,8 +67,36 @@ if __name__ == "__main__":
     from Location import Location
     scene = Scene()
     scene.setLocation(Location(randomName("location")))
-    scene.setPlayerList([randomName("character"), randomName("character"), randomName("character")])
+
+    from Player import Player, PlayerClass
+    from CombatCard import CombatCard
+    from PlayerActions import ExecutableAction
+    from Enemy import Enemy, EnemyClass
+
+    playerClass1 = PlayerClass("custom", 30)
+    exeAct1 = ExecutableAction("Attack")
+    cc1 = CombatCard("Basic Attack", [], exeAct1, 15)
+    playerClass1.addCombatCard(cc1)
+    player1 = Player("Rus", playerClass1, None)
+
+    scene.addPlayer(player1)
     scene.setLevel(1)
     scene.setEvent("Nice event")
 
-    print(f"Location: {scene.getLocation().getName()}\nPlayer list: {scene.getPlayerList()}\nLevel: {scene.getLevel()}\nEvent: {scene.getEvent()}")
+    print(f"Location: {scene.getLocation().getName()}")
+    print(f"Player list: {scene.getPlayerList()[0].getName()}")
+    print(f"Level: {scene.getLevel()}")
+    print(f"Event: {scene.getEvent()}\n")
+
+    enemyClass1 = EnemyClass("custom", 40)
+    enemy1 = Enemy("Cunt", enemyClass1, None)
+    print(f"\nEnemy: {enemy1.getName()}")
+
+    cc2 = CombatCard("Basic Attack", [], exeAct1, 15)
+    enemy1.addCombatCard(cc2)
+    cc1.addTarget(enemy1)
+    cc1.queue() #FIXME: 'Enemy' object has no attribute 'actionsQueue' - except it does
+
+    print(f"Health: {enemy1.getHealth()}/{enemy1.getMaxHealth}")
+    enemy1.executeEffects()
+    print(f"Health: {enemy1.getHealth()}/{enemy1.getMaxHealth}")
