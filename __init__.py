b7110ea7862e93c87be40c77e2098b637dac88c6
DefinitelyRus <definitelyrus@gmail.com>
2022-01-30 19:15:20 +0800

    Removed new-line from output.

--- __init__.py
+++ __init__.py
@@ -6,6 +6,6 @@ Created on 10 Mar 2021
 from marvad import Game
 game = Game.createGame("Rus")
 
-print(f"\nTotal number of scenes: {len(game.getSceneList())}")
+print(f"Total number of scenes: {len(game.getSceneList())}")
 while True: pass
 #print(Game.__doc__)
